c9f8bb29d4c17fec16635eff26b86f2a3520af5e
Diamondluxe <hehediamondluxe@gmail.com>
2026-06-07 20:18:08 +0500

    Variables done..

---
+++ Variables.py
@@ -0,0 +1,26 @@
+a=1
+b=2
+c=3
+a1=10
+b1=20
+c1=30
+A=100
+B=200
+C=300
+intro="Welcome to Diamond Luxe.. ^-^"
+name="Dua"
+last_name="Sheikh"
+Class="Python"
+Exit="Thank you for visiting Diamond Luxe.. ^-^"
+print(intro)
+print("Wanna know about me??")
+print("Okay I got you.. :)")
+print("My name is "+name+" "+last_name)
+print("I am learning "+Class)
+print("Abhi ke liye itna hi..")
+print(c)
+print(b)
+print(a)
+print("...")
+print("Bie Bieeeee..")
+print(Exit)
