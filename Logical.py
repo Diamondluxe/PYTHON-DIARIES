a82683c851f724a963e8dd20ac331199f13cff9a
Diamondluxe <hehediamondluxe@gmail.com>
2026-06-13 22:34:12 +0500

    Logical operator done

---
+++ Logical.py
@@ -0,0 +1,12 @@
+age=18
+if age >= 18 and age < 65:
+    print("You can vote.")
+
+marks = 82
+dev = 2 
+if marks >= 80 or dev == 1:
+    print("You are eligible.")
+
+color = "red"
+if not color == "blue":
+    print("The color is not blue.")
