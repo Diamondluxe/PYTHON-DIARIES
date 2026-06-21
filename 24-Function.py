512ac0ad9e5b6b3f401e97686e6e1cff87fad2e7
Diamondluxe <hehediamondluxe@gmail.com>
2026-06-21 17:06:48 +0500

    Functions....

--- 24-Function.py
+++ 24-Function.py
@@ -6,4 +6,17 @@ greet()
 def wishing():
     print("Happy Birthday ^-^")
 
+
 wishing()
+
+def Name(name):
+    print("I am",name)
+
+
+Name("Dua Sheikh")
+
+def Age(age):
+    print("My age is",age)
+
+
+Age(18)
