d51194dd25725115719a606128d55485f8163e62
Diamondluxe <hehediamondluxe@gmail.com>
2026-06-23 20:48:47 +0500

    Assignment 1 completed

---
+++ Assignment.py
@@ -0,0 +1,78 @@
+#Question 1
+
+Name = "Ali"
+Age = 22
+CGPA = 3.75
+
+print("Name:", Name)
+print("Age:", Age)
+print("CGPA:", CGPA)
+
+#Question 2
+
+num1 = 25
+num2 = 15
+
+print("Sum: ", num1 + num2)
+print("Difference: ", num1 - num2)
+print("Product: ", num1 * num2)
+
+#Question 3
+
+temperature =36.5
+print(temperature)
+print(type(temperature))
+
+#Question 4
+
+first_name = "Dua"
+last_name = "Sheikh"
+full_name = first_name + " " + last_name
+print(full_name)
+
+#Question 5
+
+length = 10
+width = 5
+area = length * width
+print("Area of rectangle is:", area)
+
+#Question 6
+
+english = 85
+math = 90
+science = 85
+total = english + math + science
+average = total / 3
+print("Total marks:", total)
+print("Average marks:", average)
+
+#Question 7
+
+salary = 50000
+salary = float(salary)
+print("Salary:", salary)
+print("Data Type:",type(salary))
+
+#Question 8
+
+name = "Sara"
+age = 21
+
+#Question 9
+
+price = 1200
+quantity = 3
+total = price * quantity
+print("Total price is:", total)
+
+#Question 10
+
+Product_Name = "Laptop"
+Price = 50000
+Quantity = 2
+Total_Bill = Price * Quantity
+print("Product:", Product_Name)
+print("Price:", Price)
+print("Quantity:", Quantity)
+print("Total Bill:", Total_Bill)
