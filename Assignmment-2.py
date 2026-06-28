f3c8cd67de6cb75b526c02275cac234a7dcf234b
Diamondluxe <hehediamondluxe@gmail.com>
2026-06-28 16:32:05 +0500

    Assignment 2 is completed

---
+++ Assignmment-2.py
@@ -0,0 +1,91 @@
+#Question 1
+
+Name ="Dua Sheikh"
+print(Name)
+
+#Question 2
+
+a=5
+b=10
+print(a+b)
+
+#Question 3
+
+Age = 18
+print(Age)
+
+#Question 4
+
+Integer = 67
+Float = 3.14
+String = "Hello, World!"
+print(Integer)
+print(Float)
+print(String)
+
+#Question 5
+
+IsActive = True
+print(IsActive)
+
+#Question 6
+
+num = int(input("Enter a number: "))
+if num>=10:
+    print("Big")
+else:
+    print("Small")
+
+#Question 7
+
+number  = int(input("Enter a number: "))
+if number%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#Question 8
+
+Age = int(input("Enter your age: "))
+if Age>=18:
+    print("Adult.")
+else:
+    print("Minor.")
+
+#Question 9
+
+for i in range(1,6):
+    print(i)
+
+#Question 10
+
+for i in range(5):
+    print("Dua Sheikh")
+
+#Question 11
+
+Num = 2
+for i in range(5):
+    print(Num)
+    Num+=2
+
+#Question 12
+
+i = 1 
+while i<=5:
+    print(i)
+    i+=1
+
+#Question 13
+
+a = 5
+while a>=1:
+    print(a)
+    a-=1
+
+#Question 14
+
+def hello():
+    print("Hello, World!")
+
+hello()
